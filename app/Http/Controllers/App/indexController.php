74982ceacec207d89feec195e1f2ecafe260f367
lippan <lipan@sanygroup.com>
2020-07-26 20:23:45 +0800

    增加列表检索

--- app/Http/Controllers/App/indexController.php
+++ app/Http/Controllers/App/indexController.php
@@ -12,6 +12,78 @@ use Illuminate\Support\Facades\DB;
 class IndexController  extends BaseController
 {
 
+    public function lists(Request $request)
+    {
+        $data["start_time"]     = $request->post("start_time");
+        $data["end_time"]       = $request->post("end_time");
+        $page                   = $request->post("page",1);
+        $page_nums              = $request->post("page_nums",20);
+
+        $where =[];
+        $where1 = null;
+        if(!empty($data["start_time"]))
+        {
+            $where[]    = ["create_time",">",$data["start_time"]];
+            $where1[]   = ["answer_update_time",">",$data["start_time"]];
+        }
+        if(!empty($data["end_time"]))
+        {
+            $where[]    = ["create_time","<",$data["end_time"]];
+            $where1[]   = ["answer_update_time","<",$data["end_time"]];
+        }
+
+
+        $offset     = ($page-1) * $page_nums;
+
+        $count = DB::table("business")
+            ->where($where)
+            ->when(isset($where1),function ($query) use ($where1){
+                $query->orWhere($where1);
+            })
+            ->count();
+
+        if(empty($count))
+            return ["count"=>0,"list"=>""];
+
+        $list = DB::table("business")
+            ->where($where)
+            ->when(isset($where1),function ($query) use ($where1){
+                $query->orWhere($where1);
+            })
+            ->orderby('a.create_time',"desc")
+            ->offset($offset)
+            ->limit($page_nums)
+            ->count();
+
+        $data["count"] = $count;
+        $data["list"]  = $list;
+
+        $this->success($data);
+    }
+
+
+    public function getDateNum(Request $request)
+    {
+
+        $date     = $request->post("date");
+
+        $where =[];
+        if(!empty($date))
+        {
+            $where[]   = ["answer_update_time",">",$date];
+            $where[]   = ["answer_update_time","<",$date." 23:59:59"];
+        }
+        else
+            $this->failed("日期不能为空");
+
+        $count = DB::table("business")
+            ->where($where)
+            ->whereNotNull("answer")
+            ->count();
+
+        $this->success(["count"=>$count]);
+    }
+
     public function notice(Request $request)
     {
         $data   = $request->all();
@@ -85,14 +157,38 @@ class IndexController  extends BaseController
             if(time()> strtotime($row->end_time))
                 $this->failed("您访问的页面过期了");
 
-            $data = [
-                   "answer" =>$row->answer.":".$answer
+            if(strpos($row->answer,$answer) !== false)//出现重复答案
+            {
+                $data = [
+                    "answer"                =>$row->answer,
+                    "answer_update_time"    => date("Y-m-d H:i:s")
+                ];
+            }
+            else
+                $data = [
+                    "answer"                =>$row->answer."-".$answer,
+                    "answer_update_time"    => date("Y-m-d H:i:s")
             ];
+
+
             DB::table("business")->where("mobile",$id)->update($data);
 
             $this->success($row);
         }
         else
             $this->failed("请求失败");
+    }
+
+    public function mobile(Request $request)
+    {
+        $id   = $request->post("id");
+        if(empty($id))
+            $this->failed("参数不能为空");
+
+        $row = DB::table("business")->where("mobile",$id)->first();
+        if(empty($row))
+            $this->failed("记录不存在");
+
+        $this->success($row);
     }
 }
--- routes/app.php
+++ routes/app.php
@@ -3,6 +3,9 @@
 Route::match(["options","post"],'/app/index/notice', 'App\IndexController@notice');//首页列表
 Route::match(["options","post"],'/app/index/init', 'App\IndexController@init');//首页列表
 Route::match(["options","post"],'/app/index/save', 'App\IndexController@save');//首页列表
+Route::match(["options","post"],'/app/index/mobile', 'App\IndexController@mobile');//首页列表
+Route::match(["options","post"],'/app/index/lists', 'App\IndexController@lists');//首页列表
+Route::match(["options","post"],'/app/index/get_date_num', 'App\IndexController@getDateNum');//首页列表
 
 Route::namespace('App')->middleware(["token","api"])->group(function () {
 
